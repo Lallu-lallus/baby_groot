340044e91b1fec0a247d4cada3c68866235b3c74
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-05 11:05:03 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -4,15 +4,14 @@ class Presets(object):
     CAPTION_TEXT_DOC = "\n\n<b>File Name:</b> {}\n\n<b>Format:</b> {}\n<b>Size:</b> {}"
     CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
     ASK_PM_TEXT = "<b>Click the below button</b>"
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>I can help you getting movies from</code> @MovieKeralam. " \
+    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>I Am a file send bot just add me to your group and. Make me an admin then Am send files in ur group. " \
                    "<code>Just Keep this message live Here</code>😉\n\n" \
                    "<b>My code can be seen: </b><a href='https://github.com/m4mallu/gofilesbot'> HERE</a>"
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
     MSG_FOR_PIN = "<b>For getting medias from here..</b>\n\n🔛 <code>Please start</code> @{} <code>in PM\n\n" \
                   "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel !</code>"
 
-    BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
-                  "available in our DB !</code>"
+    BOT_PM_TEXT = "coming soon..."
     PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
     MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳</code>"
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available " \
