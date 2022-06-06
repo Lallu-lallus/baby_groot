56a617ab0b7501939e7ca865505cfcccad5991f6
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-06 14:04:13 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -12,7 +12,7 @@ class Presets(object):
     BOT_PM_TEXT = "coming soon..."
     PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
     MEDIA_SEND_TEXT =  "Hᴇʏ.. <b>{}</b>\n\n " \
-                        "<code>sᴜᴄᴄᴇssғᴜʟʟʏ sᴇɴᴅ ʏᴏᴜʀ ᴘᴍ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ ᴍᴏᴠɪᴇs</code>"
+                        "<code>ғɪʟᴇ sᴜᴄᴄᴇssғᴜʟʟʏ sᴇɴᴅ ʏᴏᴜʀ ᴘᴍ. ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ ᴍᴏᴠɪᴇs</code>"
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
