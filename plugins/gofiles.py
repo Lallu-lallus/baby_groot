4c53137f497104379ccd22681764edc27e42e641
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-06 15:55:45 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -128,10 +128,8 @@ async def query_mgs(client: Bot, message: Message):
             return
         if user_message.keys():
             try:
-                imdb=await get_poster(search)
-                if imdb and imdb.get('poster'):
                 await client.send_photo(
-                    photo=imdb.get('poster'),
+                    photo="https://telegra.ph/file/b8f77a8a40b407b4c9a28.jpg",
                     chat_id=message.chat.id,
                     caption=Presets.MEDIA_SEND_TEXT.format(message.from_user.first_name),
                     reply_to_message_id=user_message[id],
