dd3dc7b34ed38c07b01a45428d9eb91b5eb8ca75
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-07 11:55:54 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,4 +1,19 @@
-await client.send_message(
+import re
+import os
+import time
+import random
+from bot import Bot
+from presets import Presets
+from base64 import b64decode
+from helper.file_size import get_size
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+await client.send_photo(
+            photo=random.choice(vid),
             caption=presete.ABOUT_TXT.format(query.from_user.mention),
             chat_id=query.message.chat.id,
             reply_markup=reply_markup,
