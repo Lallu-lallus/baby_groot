d03a0770ca13e21ed4259ed6b573648315e61002
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-07 14:01:43 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -11,11 +11,12 @@ from pyrogram.errors import FloodWait
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(filters.private & filters.incoming & filters.command("about"))
+@Client.on_message(filters.private & filters.text)
 async def bot_pm(client: Bot, message: Message):
-await client.send_message(
-            text=presets.ABOUT_TXT.format(query.from_user.mention),
-            chat_id=query.message.chat.id,
+    if message.text == "/about":
+        await client.send_message(
+            chat_id=message.chat.id,
+            caption=Presets.ABOUT_TEXT.format(message.from_user.first_name),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -23,7 +24,7 @@ await client.send_message(
                     ],
                     [
                         InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/LSBOTZ_UPDATE"),
-                        InlineKeyboardButton("ʜᴇʟᴘ", url="https://t.me/EDIT_REPO")
+                        InlineKeyboardButton("ᴄʀᴇᴀᴛᴏʀ", url="https://t.me/lallu_tg")
                     ],
                     [
                         InlineKeyboardButton("Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ", url="https://t.me/Ls_filesendbot?startgroup=true")
@@ -32,4 +33,3 @@ await client.send_message(
             )
            
         )
-        
