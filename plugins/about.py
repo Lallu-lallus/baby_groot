f8ea49994173e95473dc7db10e66fdfafa86a3fe
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-07 11:53:28 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,4 +1,4 @@
 await client.send_message(
-            text=script.ABOUT_TXT.format(query.from_user.mention),
+            caption=presete.ABOUT_TXT.format(query.from_user.mention),
             chat_id=query.message.chat.id,
             reply_markup=reply_markup,
