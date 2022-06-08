06788e9f26e90dbc0a30bc88a87c72319fb93bf7
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-08 14:19:55 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -168,5 +168,5 @@ async def query_mgs(client: Bot, message: Message):
                 try:
                     await msg1.delete()
                     await message.delete()
-            except Exception:
-                pass
+                except Exception:
+                    pass
