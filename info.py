8c6e83f6671bc59d84623bbfc4eb2e230c567126
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-06-09 12:36:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,15 @@
 import re
 from os import environ
 
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
 
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
